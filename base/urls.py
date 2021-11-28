76415fa335a7d0a9d53cb7bd921842059659b084
Ankit Guha <75986219+ankit11hab@users.noreply.github.com>
2021-11-28 10:35:15 +0530

    Standings table added

--- base/urls.py
+++ base/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('question/<int:pk>', views.detail_view, name='question-detail'),
+    path('answers/<int:pk>', views.answer_form, name='answer-form'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Question
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from users.models import Submission
 from .forms import SubmissionForm
 
@@ -12,6 +13,18 @@ def home(request):
 
 @login_required
 def detail_view(request,pk):
+    question = Question.objects.get(pk = pk)
+    all_submissions = Submission.objects.filter(submitted=True)
+    if Submission.objects.filter(question=question, user = request.user).first():
+        submission = Submission.objects.filter(question=question, user = request.user).first()
+    else:
+        submission = Submission(question=question, submitted=False, submitted_answer = "", user = request.user)
+        submission.save()
+    return render(request, 'base/question_detail.html', {'question':question,'submission':submission,'all_submissions':all_submissions})
+
+
+@login_required
+def answer_form(request,pk):
     question = Question.objects.get(pk = pk)
     if request.method=='POST':
         form = SubmissionForm(request.POST)
@@ -26,8 +39,12 @@ def detail_view(request,pk):
                 if question.answer_key[i-1]==form[txt].value():
                     marks+=question.marks_per_question
         
-        submission = Submission(question=question, marks_obtd = marks, submitted_answer = ans, user = request.user)
+        submission = Submission.objects.filter(question=question, user = request.user).first()
+        submission.marks_obtd = marks
+        submission.submitted = True
+        submission.submitted_answer = ans
         submission.save()
+        return redirect('question-detail', pk)
     else:
         form = SubmissionForm()
-    return render(request, 'base/question_detail.html', {'question':question, 'form':form})
+    return render(request, 'base/answer_form.html', {'question':question,'form':form})
