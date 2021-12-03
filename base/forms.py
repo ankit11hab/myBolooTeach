d516c4233477af24590c15ceedfc29d4a1049882
danshCode <soumadipdas18@yahoo.com>
2021-12-03 16:01:41 +0530

    number of questions made dynamic

--- base/forms.py
+++ base/forms.py
@@ -1,35 +1,22 @@
 from django import forms
 
+CHOICES = [('A', 'A'),
+           ('B', 'B'),
+           ('C', 'C'),
+           ('D', 'D')]
+
+
 class SubmissionForm(forms.Form):
-    CHOICES=[('A','A'),
-         ('B','B'),
-         ('C','C'),
-         ('D','D')]
-    question1 = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=False)
-    question2 = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=False)
-    question3 = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=False)
-    question4 = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=False)
-    question5 = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=False)
-    question6 = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=False)
-    question7 = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=False)
-    question8 = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=False)
-    question9 = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=False)
-    question10 = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=False)
-    question11 = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=False)
-    question12 = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=False)
-    question13 = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=False)
-    question14 = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=False)
-    question15 = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=False)
-    question16 = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=False)
-    question17 = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=False)
-    question18 = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=False)
-    question19 = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=False)
-    question20 = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, required=False)
+    def __init__(self, n,  *args, **kwargs):
+        super(SubmissionForm, self).__init__(*args, **kwargs)
+        for i in range(0, n):
+            self.fields["Question %d" % (i+1)] = forms.ChoiceField(
+                choices=CHOICES, widget=forms.RadioSelect, required=False)
+
 
 class ProfileUpdateForm(forms.Form):
-     first_name = forms.CharField()
-     last_name = forms.CharField()
-     classs = forms.IntegerField()
-     mobile_no = forms.CharField()
-     school = forms.CharField()
-
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    classs = forms.IntegerField()
+    mobile_no = forms.CharField()
+    school = forms.CharField()
--- base/views.py
+++ base/views.py
@@ -6,19 +6,22 @@ from users.models import Submission, Profile
 from .forms import SubmissionForm, ProfileUpdateForm
 
 # Create your views here.
+
+
 @login_required
 def home(request):
-    user_profile = Profile.objects.filter(user = request.user).first()
+    user_profile = Profile.objects.filter(user=request.user).first()
     # if user_profile.first_name == "" or user_profile.mobile_no == "" or user_profile.classs==0:
     #     return redirect('profile_update')
     if not user_profile:
         return redirect('profile_update')
     questions = Question.objects.all()
-    return render(request, 'base/home.html',{'questions':questions,'profile':user_profile})
+    return render(request, 'base/home.html', {'questions': questions, 'profile': user_profile})
+
 
 @login_required
 def profile_update(request):
-    if request.method=='POST':
+    if request.method == 'POST':
         form = ProfileUpdateForm(request.POST)
         print(form['first_name'].value())
         first_name = form['first_name'].value()
@@ -26,47 +29,53 @@ def profile_update(request):
         classs = form['classs'].value()
         mobile_no = form['mobile_no'].value()
         school = form['school'].value()
-        profile = Profile(user=request.user, first_name = first_name, last_name=last_name, classs=classs, mobile_no=mobile_no, school=school)
+        profile = Profile(user=request.user, first_name=first_name,
+                          last_name=last_name, classs=classs, mobile_no=mobile_no, school=school)
         profile.save()
         return redirect('home')
     else:
         form = ProfileUpdateForm()
-    return render(request, 'base/profile_update.html',{'form':form})
+    return render(request, 'base/profile_update.html', {'form': form})
+
 
 @login_required
-def detail_view(request,pk):
-    question = Question.objects.get(pk = pk)
-    all_submissions = Submission.objects.filter(question = question,submitted=True)
-    if Submission.objects.filter(question=question, user = request.user).first():
-        submission = Submission.objects.filter(question=question, user = request.user).first()
+def detail_view(request, pk):
+    question = Question.objects.get(pk=pk)
+    all_submissions = Submission.objects.filter(
+        question=question, submitted=True)
+    if Submission.objects.filter(question=question, user=request.user).first():
+        submission = Submission.objects.filter(
+            question=question, user=request.user).first()
     else:
-        submission = Submission(question=question, submitted=False, submitted_answer = "", user = request.user)
+        submission = Submission(
+            question=question, submitted=False, submitted_answer="", user=request.user)
         submission.save()
-    return render(request, 'base/question_detail.html', {'question':question,'submission':submission,'all_submissions':all_submissions})
+    return render(request, 'base/question_detail.html', {'question': question, 'submission': submission, 'all_submissions': all_submissions})
 
 
 @login_required
-def answer_form(request,pk):
-    question = Question.objects.get(pk = pk)
-    if request.method=='POST':
-        form = SubmissionForm(request.POST)
+def answer_form(request, pk):
+    question = Question.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = SubmissionForm(question.number_of_question,request.POST)
         ans = ""
         marks = 0
-        for i in range(1,21):
-            txt = "question"+str(i)
-            if form[txt].value()==None:
+        for i in range(1, question.number_of_question):
+            txt = "Question "+str(i)
+            if form[txt].value() == None:
                 ans = ans + " "
             else:
                 ans = ans+form[txt].value()
-                if question.answer_key[i-1]==form[txt].value():
-                    marks+=question.marks_per_question
-        
-        submission = Submission.objects.filter(question=question, user = request.user).first()
+                if question.answer_key[i-1] == form[txt].value():
+                    marks += question.marks_per_question
+
+        submission = Submission.objects.filter(
+            question=question, user=request.user).first()
         submission.marks_obtd = marks
         submission.submitted = True
         submission.submitted_answer = ans
         submission.save()
         return redirect('question-detail', pk)
     else:
-        form = SubmissionForm()
-    return render(request, 'base/answer_form.html', {'question':question,'form':form})
+        form = SubmissionForm(n=question.number_of_question)
+    return render(request, 'base/answer_form.html', {'question': question, 'form': form})
