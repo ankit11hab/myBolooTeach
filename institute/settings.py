c37592384a65ca20bc22a8c5f964b9c752f87473
danshCode <soumadipdas18@yahoo.com>
2021-12-04 12:33:10 +0530

    Forget password feature added

--- institute/settings.py
+++ institute/settings.py
@@ -145,3 +145,9 @@ MESSAGE_TAGS = {
     messages.WARNING: 'alert-warning',
     messages.ERROR: 'alert-danger',
 }
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'schedulerevent9@gmail.com'
+EMAIL_HOST_PASSWORD = 'qigpglvkwjrldryy'
