2766d2d1f553caaa891a15139cc07262afb863a7
Ankit Guha <75986219+ankit11hab@users.noreply.github.com>
2021-12-05 10:45:25 +0530

    fixes #19

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from base.models import Question
+import datetime
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -9,6 +10,7 @@ class Submission(models.Model):
     marks_obtd = models.IntegerField(default=0)
     submitted = models.BooleanField(default=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_of_submission = models.DateField(default=datetime.date.today)
 
     def __str__(self):
         return f'{self.id}'
